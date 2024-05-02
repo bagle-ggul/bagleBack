c705c2b98868a2a8bf6a56b4ec8efe711eac783c
서새찬 <chan4760@gmail.com>
2024-05-02 20:36:30 +0900

    init(): init dongbanza project

---
+++ src/main/java/com/suhsaechan/dongbanza/DongbanzaApplication.java
@@ -0,0 +1,13 @@
+package com.suhsaechan.dongbanza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongbanzaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DongbanzaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/suhsaechan/dongbanza/DongbanzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhsaechan.dongbanza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongbanzaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
