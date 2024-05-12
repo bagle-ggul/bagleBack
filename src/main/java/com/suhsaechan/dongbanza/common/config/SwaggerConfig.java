8ae5421714bd9172684505348d5cf66a245f6264
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-05-12 21:43:16 +0900

    feat(api-docs): Swagger API 문서 작성
    - MemberControllerDocs 인터페이스에 signup, login 메소드 추가하여 회원 가입과 로그인 API 문서화
    
    - TokenControllerDocs 인터페이스에 createNewAccessToken 메소드 추가하여 AccessToken 재발급 API 문서화
    
    - SwaggerConfig 클래스에 OpenAPI 설정 추가하여 API 보안 스키마와 인증 요구사항 정의

---
+++ src/main/java/com/suhsaechan/dongbanza/common/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.suhsaechan.dongbanza.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+    info = @Info(title = "동반자 DBZ",
+        description = "동네 반려동물 찾자",
+        version = "0.1v"))
+@Configuration
+public class SwaggerConfig {
+  @Bean
+  public OpenAPI openAPI(){
+    SecurityScheme apiKey = new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP)
+        .in(SecurityScheme.In.HEADER)
+        .name("Authorization")
+        .scheme("bearer")
+        .bearerFormat("JWT");
+
+    SecurityRequirement securityRequirement = new SecurityRequirement()
+        .addList("Bearer Token");
+
+    return new OpenAPI()
+        .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+        .addSecurityItem(securityRequirement);
+  }
+}
+
