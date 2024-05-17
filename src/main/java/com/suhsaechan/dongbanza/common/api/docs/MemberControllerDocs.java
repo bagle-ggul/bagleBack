4e7a81bf6088b94eca50c552bd092fccc8542535
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-05-17 10:56:47 +0900

    docs(api): Member Token Controller API 구성 수정추가

--- src/main/java/com/suhsaechan/dongbanza/common/api/docs/MemberControllerDocs.java
+++ src/main/java/com/suhsaechan/dongbanza/common/api/docs/MemberControllerDocs.java
@@ -1,5 +1,6 @@
 package com.suhsaechan.dongbanza.common.api.docs;
 
+import com.suhsaechan.dongbanza.common.jwt.dto.CustomUserDetails;
 import com.suhsaechan.dongbanza.member.dto.request.MemberLoginForm;
 import com.suhsaechan.dongbanza.member.dto.request.MemberSignUpForm;
 import com.suhsaechan.dongbanza.member.dto.response.MemberDto;
@@ -9,25 +10,35 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Tag(name = "회원 API", description = "회원 관련 API")
 public interface MemberControllerDocs {
 
-  @Operation(summary = "회원 가입",
-      description = "새로운 회원 정보를 등록한다.",
+  @Operation(summary = "로그인", description = "회원 로그인을 처리한다.",
+      responses = {
+          @ApiResponse(responseCode = "200", description = "로그인 성공",
+              content = @Content(schema = @Schema(implementation = MemberLoginResponse.class)))
+      })
+  ResponseEntity<MemberLoginResponse> login(@RequestBody MemberLoginForm form);
+
+  @Operation(summary = "회원 가입", description = "새로운 회원 정보를 등록한다.",
       responses = {
           @ApiResponse(responseCode = "200", description = "회원 등록 성공",
               content = @Content(schema = @Schema(implementation = MemberDto.class)))
       })
   ResponseEntity<MemberDto> signup(@RequestBody MemberSignUpForm form);
 
-  @Operation(summary = "로그인",
-      description = "회원 로그인을 처리한다.",
+  @Operation(summary = "내 정보 조회", description = "로그인된 사용자의 정보를 조회한다.",
       responses = {
-          @ApiResponse(responseCode = "200", description = "로그인 성공",
-              content = @Content(schema = @Schema(implementation = MemberLoginResponse.class)))
-      })
-  ResponseEntity<MemberLoginResponse> login(@RequestBody MemberLoginForm form);
+          @ApiResponse(responseCode = "200", description = "조회 성공",
+              content = @Content(schema = @Schema(implementation = MemberDto.class))),
+          @ApiResponse(responseCode = "403", description = "접근 거부")
+      }
+  )
+  ResponseEntity<MemberDto> getMemberById(
+      @AuthenticationPrincipal CustomUserDetails userDetails);
 }
--- src/main/java/com/suhsaechan/dongbanza/common/config/SwaggerConfig.java
+++ src/main/java/com/suhsaechan/dongbanza/common/config/SwaggerConfig.java
@@ -2,21 +2,29 @@ package com.suhsaechan.dongbanza.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
     info = @Info(title = "동반자 DBZ",
         description = "동네 반려동물 찾자",
-        version = "0.1v"))
+        version = "0.1v"),
+    servers = {
+        @Server(url = "https://api.suhsaechan-dbz.co.kr", description = "Main Sever"),
+        @Server(url = "http://localhost:8080", description = "Local Server")
+    }
+)
 @Configuration
 public class SwaggerConfig {
+
   @Bean
-  public OpenAPI openAPI(){
+  public OpenAPI openAPI() {
     SecurityScheme apiKey = new SecurityScheme()
         .type(SecurityScheme.Type.HTTP)
         .in(SecurityScheme.In.HEADER)
@@ -29,7 +37,15 @@ public class SwaggerConfig {
 
     return new OpenAPI()
         .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
-        .addSecurityItem(securityRequirement);
+        .addSecurityItem(securityRequirement)
+        .servers(List.of(
+            new io.swagger.v3.oas.models.servers.Server()
+                .url("http://localhost:8080")
+                .description("Local Server"),
+            new io.swagger.v3.oas.models.servers.Server()
+                .url("https://api.suhsaechan-dbz.co.kr")
+                .description("Main Server")
+            )
+        );
   }
 }
-
