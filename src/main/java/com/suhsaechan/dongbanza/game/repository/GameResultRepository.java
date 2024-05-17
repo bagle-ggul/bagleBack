2d1930494f6aa7dc0847963b02c956cf09b29958
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-05-17 19:45:44 +0900

    feat(game): gameResult 서비스 로직

---
+++ src/main/java/com/suhsaechan/dongbanza/game/repository/GameResultRepository.java
@@ -0,0 +1,14 @@
+package com.suhsaechan.dongbanza.game.repository;
+
+import com.suhsaechan.dongbanza.game.domain.entity.GameResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GameResultRepository extends JpaRepository<GameResult, Long> {
+  List<GameResult> findByMemberId(Long memberId);
+  Optional<GameResult> findByIdAndMemberId(Long id, Long memberId);
+}
